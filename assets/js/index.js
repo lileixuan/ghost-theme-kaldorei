fcafd20074f0bd3bf28ded1fe94202ea703ea8f5
kency <lileixuan@gmail.com>
2018-10-16 16:01:07 +0800

    增加友情链接

--- assets/js/index.js
+++ assets/js/index.js
@@ -79,7 +79,9 @@
             e.preventDefault();
             e.stopPropagation();
             // set target to anchor's "href" attribute
-            var target = $(this).attr('href');
+            // Thanks to @https://github.com/xiongchengqing fixed this bug.
+            var target = document.getElementById($(this).attr('href').split('#')[1]);
+            console.log(target);
             // scroll to each target
             $(target).velocity('scroll', {
                 duration: 500,
@@ -91,24 +93,14 @@
         // tooltip config
         $('[data-rel=tooltip]').tooltip();
 
-        // fancybox config
+        // fancybox 3.1.25 config
         $('.post-content a:has(img)').addClass('fancybox');
-        $(".fancybox").attr('rel', 'gallery-group').fancybox({
-            helpers: {
-                overlay: {
-                    css: {
-                        'background': 'rgba(0, 154, 97, 0.33)'
-                    },
-                    locked: false
-                }
-            },
-            beforeShow: function() {
-                var alt = this.element.find('img').attr('alt');
-
-                this.inner.find('img').attr('alt', alt);
-
-                this.title = alt;
-            }
+        $(".fancybox").attr('data-fancybox', 'images').fancybox({
+            selector       : '[data-fancybox="images"]',
+            loop           : true,
+            slidesToShow   : 3,
+            slidesToScroll : 3,
+            infinite       : true
         });
 
         // add archives year
